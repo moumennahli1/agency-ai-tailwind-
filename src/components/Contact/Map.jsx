523cc7de04b747a9e0e208e8477e92e1866d902e
moumennahli1 <moumennahli125@gmail.com>
2026-02-07 15:18:44 +0100

    fix the issue

--- src/components/Contact/Map.jsx
+++ src/components/Contact/Map.jsx
@@ -1,7 +1,14 @@
 export default function Map() {
   return (
-    <div className="w-screen -mx-6 h-[350px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] rounded-none overflow-hidden shadow-lg">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3324.890067983821!2d-7.5146803855896!3d33.55623245239258!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda7cd4a079e792b%3A0x5af217fe68e7b90c!2sPHARMACIE%20AGARDI!5e0!3m2!1sen!2sma!4v1770428819897!5m2!1sen!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div className="w-[650px] h-[650px] rounded-xl overflow-hidden shadow-md">
+      <iframe
+        title="Pharmacie Agardi Location"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3324.890067983821!2d-7.5146803855896!3d33.55623245239258!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda7cd4a079e792b%3A0x5af217fe68e7b90c!2sPHARMACIE%20AGARDI!5e0!3m2!1sen!2sma!4v1769967077459!5m2!1sen!2sma"
+        className="w-full h-full border-0"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      />
     </div>
   );
 }
